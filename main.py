d31a7e7a114fc9df65adbc6195ab33ebced9d3e5
hernan <haguil0122@gmail.com>
2026-05-10 14:04:37 +0200

    adding delta time

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
+FPS = 60
+
 def main():
     print("Starting Asteroids with pygame version:", pygame.version.ver)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt =  0
     while(True):
         log_state()
         for event in pygame.event.get():
@@ -15,6 +19,7 @@ def main():
         screen.fill("black")
         # last step:
         pygame.display.flip() # refresh screen
+        dt = clock.tick(FPS) / 1000
 
 
 if __name__ == "__main__":
